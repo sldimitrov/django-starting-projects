769a732e8dbdd6d21766aa338ddc528486fc1353
Slavi FE <slavidimitrov54@gmail.com>
2026-01-18 14:26:16 +0200

    init django blog

--- django-blog/blog/views.py
+++ django-blog/blog/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    posts = Post.objects.all()
+
+    data = [
+        {
+            "title": post.title,
+            "text": post.text
+        }
+        for post in posts
+    ]
+
+    return JsonResponse(data, safe=False)
+
+def detail(request, title):
+    return HttpResponse("You're looking at posts %s." % title)
+
+
+def category(request, category_id):
+    response = "You're looking at the category of post %s."
+    return HttpResponse(response % category_id)
+
+
+def tag(request, tag_id):
+    return HttpResponse("You're seeing tag %s." % tag_id)

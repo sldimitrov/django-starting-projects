d9d5de8bca679032751942335a8f1b4bb59f1d76
Slavi FE <slavidimitrov54@gmail.com>
2026-01-22 22:40:06 +0200

    added static files, images, styles and templates

--- blog_udemy/blog/views.py
+++ blog_udemy/blog/views.py
@@ -5,7 +5,7 @@ def starting_page(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
-def post_details(request):
-    pass
+def post_details(request, slug):
+    return render(request, "blog/post-detail.html")

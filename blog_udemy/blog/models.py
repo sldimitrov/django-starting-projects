c94b6961c4bff4b3fd1e5d66c9bde2387c43ce12
Slavi FE <slavidimitrov54@gmail.com>
2026-01-31 19:59:31 +0200

    dynamic(blog) add models

--- blog_udemy/blog/models.py
+++ blog_udemy/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email_address = models.EmailField(max_length=254)
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=150)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=150)
+    image_name = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    caption = models.ManyToManyField(Tag)
+    date = models.DateField()
+    slug = models.SlugField()
+    content = models.CharField(max_length=1000)

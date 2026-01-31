d377a54cd930c15fe1c48738fb3904c46014810d
Slavi FE <slavidimitrov54@gmail.com>
2026-01-31 19:48:10 +0200

    book-store finish with course section

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Author - {self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     rating = models.IntegerField(default=3)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=True)

7bf225723db2328c72f128046fab2e70c9da6c6f
Slavi FE <slavidimitrov54@gmail.com>
2026-01-31 21:41:28 +0200

    dynamic-blog add dynamic data to templates

--- blog_udemy/blog/models.py
+++ blog_udemy/blog/models.py
@@ -5,10 +5,19 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
-    email_address = models.EmailField(max_length=254)
+    email_address = models.EmailField(max_length=254, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 class Tag(models.Model):
     caption = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.caption
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -16,6 +25,9 @@ class Post(models.Model):
     image_name = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     caption = models.ManyToManyField(Tag)
-    date = models.DateField()
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=50, db_index=True)
     content = models.CharField(max_length=1000)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} with Author - '{self.author}'"

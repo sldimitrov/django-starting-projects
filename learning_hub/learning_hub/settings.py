822afa49775d0d7b5974f26422e14c3193e22985
Slavi FE <slavidimitrov54@gmail.com>
2026-02-01 20:06:02 +0200

    add cors and rest framework to BE

--- learning_hub/learning_hub/settings.py
+++ learning_hub/learning_hub/settings.py
@@ -29,12 +29,16 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://localhost:3000",
+    "http://127.0.0.1:5173",
 ]
+
+CORS_ALLOW_ALL_ORIGINS=True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'corsheaders',
     'courses',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
